ea6c5bfbdf8f2e683fea475c4e01e2766aa3f51f
kcoughlan78 <kcoughlan78@gmail.com>
2012-11-21 17:10:54 +0000

    changes made to remove dupes from store view

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,13 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
-  end
+    @products = Product.search(params[:search])
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @products }
+    end
+
+
+    end
 end
