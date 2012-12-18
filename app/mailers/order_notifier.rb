dc000f350246e61dba4c951ec47d5958046918f2
root <root@dave-lm.(none)>
2012-12-18 21:48:33 +0000

    app/assets/javascripts/orders.js.coffee app/assets/stylesheets/orders.css.scss app/controllers/orders_controller.rb app/helpers/orders_helper.rb app/mailers/order_notifier.rb app/mailers/user_mailer.rb app/models/order.rb app/views/order_notifier/received.text.erb app/views/order_notifier/shipped.text.erb app/views/orders/_form.html.erb app/views/orders/edit.html.erb app/views/orders/index.html.erb app/views/orders/new.html.erb app/views/orders/show.html.erb app/views/user_mailer/welcome_email.html.erb db/migrate/20121217204358_create_orders.rb db/migrate/20121217204409_add_order_id_to_line_item.rb test/fixtures/orders.yml test/functional/order_notifier_test.rb test/functional/orders_controller_test.rb test/functional/user_mailer_test.rb test/unit/helpers/orders_helper_test.rb test/unit/order_test.rb

---
+++ app/mailers/order_notifier.rb
@@ -0,0 +1,25 @@
+class OrderNotifier < ActionMailer::Base
+  default from: "xgamesstore@gmail.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.received.subject
+  #
+  def received(order)
+    @order = order
+
+    mail to: order.email, subject: 'XGames Order Reecieved'
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.shipped.subject
+  #
+  def shipped(order)
+    @order = order
+
+    mail to: order.email, subject: 'Xgames Order Shipped'
+  end
+end
