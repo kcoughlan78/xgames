9a50ef57f311abfa3455ba3da13891236553719f
kcoughlan78 <kcoughlan78@gmail.com>
2012-12-19 19:02:17 +0000

    typo fix in product/index

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(:version => 20121218210605) do
     t.datetime "created_at",                :null => false
     t.datetime "updated_at",                :null => false
     t.integer  "quantity",   :default => 1
+    t.integer  "order_id"
   end
 
   create_table "list_items", :force => true do |t|
@@ -32,6 +33,15 @@ ActiveRecord::Schema.define(:version => 20121218210605) do
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
     t.integer  "quantity"
+  end
+
+  create_table "orders", :force => true do |t|
+    t.string   "name"
+    t.text     "address"
+    t.string   "email"
+    t.string   "pay_type"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "parcels", :force => true do |t|
